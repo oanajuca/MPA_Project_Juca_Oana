14bf8a9559075250fe774da7f5610de15309061f
ASUS <ASUS@DESKTOP-A5NMFKB>
2023-01-09 08:57:09 +0200

    Add project files.

---
+++ MPA_Project_Juca_Oana/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MPA_Project_Juca_Oana.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
